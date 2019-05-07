c56097f89dfe6d8e65d6c008cb135e5bfe5124cc
Rigsto <rigsto@users.noreply.github.com>
2019-05-07 23:36:46 +0700

    update taken

--- takeOrder.php
+++ takeOrder.php
@@ -8,7 +8,7 @@ $driver = $db->query("SELECT id_driver FROM drivers WHERE email = '$email'");
 $fetch = mysqli_fetch_assoc($driver);
 $idDriver = $fetch['id_driver'];
 
-$update = $db->query("UPDATE taxi_order SET taken_by = $idDriver WHERE id_order = $idOrder");
+$update = $db->query("UPDATE taxi_order SET taken_by = $idDriver, taken = '1' WHERE id_order = $idOrder");
 if ($update) {
 	// $update2 = $db->query("UPDATE drivers SET check_in = '0' WHERE email = '$email'");
 	// if ($update2) {
